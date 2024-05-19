4c6f5a159aa866bb45c484fcbd75b70094f57ac7
5alidev <khalidbenhamou.dev@outlook.com>
2024-05-19 16:34:07 +0000

    template works :)

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,7 +4,8 @@ script that starts a Flask web application
 """
 
 from flask import Flask, render_template
-app = Flask("__name__")
+
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
